7ea5d8f1ff2793a7d199e0d0133e47153838d479
yamagh <hiroyuki.yamagami@gmail.com>
2024-10-30 00:08:54 +0900

    Add global `bookmarks` variable and update references
    * **bookmarks.js**
      - Define the `bookmarks` variable as a global variable
    
    * **app.js**
      - Remove the definition of the `bookmarks` variable
      - Use the global `bookmarks` variable in the `data` function
    
    * **bookmarks.html**
      - Add a script tag to include `bookmarks.js` before `app.js`

---
+++ bookmarks.js
@@ -0,0 +1,32 @@
+const bookmarks = {
+  "ChatGPT": {
+    "url": "https://chat.openai.com/chat",
+    "tags": ["🧰 Tools"],
+    "keywords": ["cg"],
+    "label": "ChatGPT"
+  },
+  "Google": {
+    "url": "https://www.google.com/search?q=%s",
+    "tags": ["🔍 Search", "🌍 Web"],
+    "keywords": ["gg", "search"],
+    "label": "Google Search"
+  },
+  "Wikipedia": {
+    "url": "https://en.wikipedia.org/wiki/%s",
+    "tags": ["📚 Education", "🌍 Web"],
+    "keywords": ["wiki", "education"],
+    "label": "Wikipedia"
+  },
+  "GitHub": {
+    "url": "https://github.com/",
+    "tags": ["👨‍💻 Development", "📦 Tools"],
+    "keywords": ["gh", "code"],
+    "label": "GitHub"
+  },
+  "Twitter": {
+    "url": "https://twitter.com/home",
+    "tags": ["🧑 SNS"],
+    "keywords": ["twitter", "social"],
+    "label": "Twitter"
+  }
+};
